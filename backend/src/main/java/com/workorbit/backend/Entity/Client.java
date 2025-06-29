1d74694e5009b56f77e932f32c318915a1c587d9
aditya <adityakotkar47@gmail.com>
2025-06-29 17:01:03 +0530

    feat: implement Client entity with auditing fields and repository interface

--- backend/src/main/java/com/workorbit/backend/Entity/Client.java
+++ backend/src/main/java/com/workorbit/backend/Entity/Client.java
@@ -1,4 +1,99 @@
 package com.workorbit.backend.Entity;
 
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
 public class Client {
+	
+	@Id
+	@GeneratedValue
+	private Long id;
+	
+	private String name;
+	private String email;
+	private String password;
+	
+	@CreatedDate
+	private LocalDateTime createdAt;
+	
+	@LastModifiedDate
+	private LocalDateTime updatedAt;
+
+	public Client() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Client(Long id, String name, String email, String password, LocalDateTime createdAt,
+			LocalDateTime updatedAt) {
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.password = password;
+		this.createdAt = createdAt;
+		this.updatedAt = updatedAt;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public LocalDateTime getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(LocalDateTime updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+
+	@Override
+	public String toString() {
+		return "Client [id=" + id + ", name=" + name + ", email=" + email + ", password=" + password + "]";
+	}
+
 }

a78e8cbfcbd8687295517ebb83767acfefe0117d
aditya <adityakotkar47@gmail.com>
2025-06-30 15:18:13 +0530

    refactor: remove redundant getters and setters from Client entity;

--- backend/src/main/java/com/workorbit/backend/Entity/Client.java
+++ backend/src/main/java/com/workorbit/backend/Entity/Client.java
@@ -43,54 +43,6 @@ public class Client {
 		this.updatedAt = updatedAt;
 	}
 
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	public LocalDateTime getCreatedAt() {
-		return createdAt;
-	}
-
-	public void setCreatedAt(LocalDateTime createdAt) {
-		this.createdAt = createdAt;
-	}
-
-	public LocalDateTime getUpdatedAt() {
-		return updatedAt;
-	}
-
-	public void setUpdatedAt(LocalDateTime updatedAt) {
-		this.updatedAt = updatedAt;
-	}
-
 	@Override
 	public String toString() {
 		return "Client [id=" + id + ", name=" + name + ", email=" + email + ", password=" + password + "]";

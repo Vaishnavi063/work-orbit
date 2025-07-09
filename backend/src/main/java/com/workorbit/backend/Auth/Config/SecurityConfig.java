0fccb5dbeff98a1e8a5d37abbe4d3a42eb00c43c
aditya <adityakotkar47@gmail.com>
2025-07-09 20:01:48 +0530

    feat: implement JWT filter for token-based request authentication

--- backend/src/main/java/com/workorbit/backend/Auth/Config/SecurityConfig.java
+++ backend/src/main/java/com/workorbit/backend/Auth/Config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.workorbit.backend.Auth.Config;
 
+import com.workorbit.backend.Auth.Filter.JwtAuthFilter;
 import com.workorbit.backend.Auth.Service.UserDetailsServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
@@ -15,6 +16,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -22,6 +24,7 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     private final UserDetailsServiceImpl userDetailsService;
+    private final JwtAuthFilter jwtAuthFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -41,7 +44,8 @@ public class SecurityConfig {
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
-                .authenticationProvider(authenticationProvider());
+                .authenticationProvider(authenticationProvider())
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }

eb79fa4d033f3dfe04b8eab5f88cd86c51828440
krishmungase <mungasekrishna8@gmail.com>
2025-07-10 23:52:17 +0530

    feat: add logo

--- frontend/src/components/shared/site-header.tsx
+++ frontend/src/components/shared/site-header.tsx
@@ -1,9 +1,11 @@
+import LogoLink from "./logo-link";
+
 const SiteHeader = () => {
   return (
     <header className="border-grid sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="px-4 sm:px-8 flex h-14 items-center mx-auto space-x-2 justify-between">
         <div className="flex items-center justify-center space-x-4 sm:space-x-8">
-          <p>Logo</p>
+          <LogoLink />
           {/* <NavLinks links={NAV_LINKS} /> */}
         </div>
         <div className="flex items-center justify-center space-x-2">

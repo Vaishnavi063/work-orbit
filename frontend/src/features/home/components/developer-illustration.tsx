d9c2b102db23344cb904cefc2722b50354663db3
krishmungase <mungasekrishna8@gmail.com>
2025-07-12 14:57:55 +0530

    feat: add url

--- frontend/src/features/home/components/developer-illustration.tsx
+++ frontend/src/features/home/components/developer-illustration.tsx
@@ -1,7 +1,10 @@
 import { Button } from "@/components/ui/button";
 import illustration from "/assests/working-man.png";
+import { useNavigate } from "react-router-dom";
 
 const DeveloperIllustration = () => {
+  const navigate = useNavigate();
+  const redirect = (url: string) => navigate(url);
   return (
     <section className="flex flex-col md:flex-row items-center justify-between px-6 py-8 dark:bg-slate-900">
       <div className="mt-8 md:mt-0 md:mr-20">
@@ -22,7 +25,10 @@ const DeveloperIllustration = () => {
           time. Whether you're a developer, designer, or writer, get paid for
           what you do best.
         </p>
-        <Button className="bg-primary text-white py-3 px-6 rounded-lg text-base hover:bg-primary/80 transition cursor-pointer">
+        <Button
+          onClick={() => redirect("/auth/sign-up")}
+          className="bg-primary text-white py-3 px-6 rounded-lg text-base hover:bg-primary/80 transition cursor-pointer"
+        >
           Become a Freelancer
         </Button>
       </div>

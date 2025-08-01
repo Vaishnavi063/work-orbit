07c705dafefda24528d432108714ac938b863984
aditya <adityakotkar47@gmail.com>
2025-08-01 17:35:52 +0530

    feat: add contact modal

--- frontend/src/apis/index.ts
+++ frontend/src/apis/index.ts
@@ -1,5 +1,6 @@
 import siteConfigs from "./site-config"
 import request from "./request";
 import { getProjectCounts } from "./categories";
+import { submitContactForm } from "./contact";
 
-export { siteConfigs, request, getProjectCounts };
+export { siteConfigs, request, getProjectCounts, submitContactForm };
